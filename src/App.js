494b628c32e090a21b20eac32237fcde6cf8bb21
Sangam Sundar <sangamsundar25@gmail.com>
2020-05-26 12:07:16 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello Healer, God bless! Life is so beautiful!!! Show gratitude!
         </p>
         <a
           className="App-link"
